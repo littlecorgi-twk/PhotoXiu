f0c4de03c105027c589a5fd01437fb50dbf1ab27
a1203991686 <a1203991686@126.com>
2020-06-29 16:59:15 +0800

    🔧feat:add ttAdSdk SplashAd

--- app/src/main/java/com/littlecorgi/photoxiu/MyMainApplication.kt
+++ app/src/main/java/com/littlecorgi/photoxiu/MyMainApplication.kt
@@ -2,6 +2,7 @@ package com.littlecorgi.photoxiu
 
 import android.app.Application
 import com.littlecorgi.photoxiu.ttadsdk.TTAdManagerHolder
+import com.littlecorgi.photoxiu.util.DisplayUtil
 
 
 class MyMainApplication : Application() {
@@ -15,5 +16,10 @@ class MyMainApplication : Application() {
         // 穿山甲SDK初始化
         //强烈建议在应用对应的Application#onCreate()方法中调用，避免出现content为null的异常
         TTAdManagerHolder.init(this)
+
+        // 初始化DisplayUtil
+        DisplayUtil.sContext = this
+        DisplayUtil.sScreenWidth = DisplayUtil.getScreenWidth()
+        DisplayUtil.sScreenHeight = DisplayUtil.getScreenHeight()
     }
 }
